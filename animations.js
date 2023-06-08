434fa8cf8866b9cd3f989bf29b1ecfaab9ff72e8
mjemborsky <michaelemborsky@gmail.com>
2023-06-08 03:05:56 -0400

    hopefully something

--- animations.js
+++ animations.js
@@ -1,37 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the link and expanded links container
+  const linkLeft = document.querySelector('.link-left');
+  const expandedLinks = document.querySelector('.expanded-links');
 
-
-function createGrid() {
-  const container = document.getElementById('grid-container');
-
-  for (let i = 0; i < numRows; i++) {
-    for (let j = 0; j < numColumns; j++) {
-      const gridItem = document.createElement('div');
-      gridItem.className = 'grid-item';
-      container.appendChild(gridItem);
-
-      gsap.fromTo(
-        gridItem,
-        {
-          x: j * 10,
-          y: i * 10,
-        },
-        {
-          x: j * 10 + getRandomOffset(),
-          y: i * 10 + getRandomOffset(),
-          duration: 0.5,
-          repeat: -1,
-          yoyo: true,
-          ease: 'power2.inOut',
-        }
-      );
-    }
-  }
-}
-
-function getRandomOffset() {
-  // Generate a random offset for the animation
-  return Math.random() * 5 - 2.5;
-}
-
-// Call the createGrid function to initialize the animation
-createGrid();
+  // Add a click event listener to the link
+  linkLeft.addEventListener('click', () => {
+    // Toggle the visibility of the expanded links
+    expandedLinks.classList.toggle('show');
+  });
+});
