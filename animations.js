ab975f68ac6411e1a562ddea6c46252120b7e310
mjemborsky <michaelemborsky@gmail.com>
2023-06-26 16:18:50 -0400

    Update animations.js

--- animations.js
+++ animations.js
@@ -24,22 +24,48 @@ function createRandomTriangle() {
   return triangle;
 }
 
-// Function to add random triangles to the container
+// Function to check if two triangles overlap
+function doTrianglesOverlap(triangle1, triangle2) {
+  var rect1 = triangle1.getBoundingClientRect();
+  var rect2 = triangle2.getBoundingClientRect();
+
+  return !(
+    rect1.right + 5 < rect2.left ||
+    rect1.left - 5 > rect2.right ||
+    rect1.bottom + 5 < rect2.top ||
+    rect1.top - 5 > rect2.bottom
+  );
+}
+
+// Function to add random triangles with a specific spacing
 function addRandomTriangles() {
   var container = document.querySelector('.container');
   var numTriangles = 50; // Adjust the number of triangles as desired
+  var triangles = [];
 
   for (var i = 0; i < numTriangles; i++) {
     var triangle = createRandomTriangle();
+    triangles.push(triangle);
+
     var containerWidth = container.offsetWidth;
     var containerHeight = container.offsetHeight;
-    var posX = getRandomNumber(0, containerWidth);
-    var posY = getRandomNumber(0, containerHeight);
-    triangle.style.top = posY + 'px';
-    triangle.style.left = posX + 'px';
+    var posX, posY;
+
+    do {
+      posX = getRandomNumber(0, containerWidth);
+      posY = getRandomNumber(0, containerHeight);
+      triangle.style.top = posY + 'px';
+      triangle.style.left = posX + 'px';
+    } while (
+      triangles.some(function (otherTriangle) {
+        return doTrianglesOverlap(triangle, otherTriangle);
+      })
+    );
+
     container.appendChild(triangle);
   }
 }
+
 
 // Call the function to add random triangles when the page loads
 window.addEventListener('load', addRandomTriangles);
