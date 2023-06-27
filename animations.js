5de9277afd38816d0181e4706742d86ea5d921c0
mjemborsky <michaelemborsky@gmail.com>
2023-06-27 18:51:17 -0400

    updates

--- animations.js
+++ animations.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Get the container element
   var container = document.querySelector(".container");
-
-  // Define an array of background image URLs
   var backgrounds = [
     "background.svg",
     "backgroundTwo.svg",
@@ -11,29 +8,26 @@ document.addEventListener("DOMContentLoaded", function() {
     "backgroundFive.svg"
   ];
 
-  // Set the initial background image
   container.style.backgroundImage = "url('" + backgrounds[0] + "')";
 
-  // Function to change the background image
   function changeBackground(index) {
-    // Set the background image
-    container.style.backgroundImage = "url('" + backgrounds[index] + "')";
+    container.style.animation = "fade-out 0.5s";
+    setTimeout(function() {
+      container.style.backgroundImage = "url('" + backgrounds[index] + "')";
+      container.style.animation = "fade-in 0.5s";
+    }, 500);
   }
 
-  // Add event listeners to links with the class "link-left"
   var linkLeft = document.querySelectorAll(".link-left");
   for (var i = 0; i < linkLeft.length; i++) {
     linkLeft[i].addEventListener("click", function() {
-      // Call the changeBackground function with the index of backgroundTwo.svg
       changeBackground(1);
     });
   }
 
-  // Add event listeners to links with the class "link-right"
   var linkRight = document.querySelectorAll(".link-right");
   for (var i = 0; i < linkRight.length; i++) {
     linkRight[i].addEventListener("click", function() {
-      // Call the changeBackground function with the index of backgroundFive.svg
       changeBackground(4);
     });
   }
