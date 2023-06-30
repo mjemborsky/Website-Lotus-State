25cb7cdc9c2d4547d26df2c11b143ddfecc987b9
mjemborsky <michaelemborsky@gmail.com>
2023-06-30 01:23:44 -0400

    blah

--- animations.js
+++ animations.js
@@ -37,16 +37,3 @@ function animateCircles(sourceSVG, targetSVG, duration) {
     }, duration);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  // Get all the links with the class "link-left"
-  var links = document.querySelectorAll('.link-left');
-
-  // Add click event listener to each link
-  links.forEach(function(link) {
-    link.addEventListener('click', function(event) {
-      event.preventDefault(); // Prevent the default link behavior
-      animateCircles('#background', '#backgroundTwo', 2000); // Adjust duration as needed
-    });
-  });
-});
