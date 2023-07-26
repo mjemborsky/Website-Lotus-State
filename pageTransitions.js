cd1ba7792c4025d4a50585001e30b2504c518d62
mjemborsky <michaelemborsky@gmail.com>
2023-07-26 11:39:58 -0400

    something

--- pageTransitions.js
+++ pageTransitions.js
@@ -50,45 +50,18 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   function animateBackground(currentBackground, targetBackground) {
-    // Fetch the Snap.svg instances of both SVGs
-    var currentSnap = Snap.select(currentBackground);
-    var targetSnap = Snap.select(targetBackground);
-    var contentElement = document.querySelector('.container');
+    // Add class for animation
+    currentBackground.classList.add('animate-background');
 
-    // Fetch all circles and positions from the current SVG
-    var currentCircles = currentSnap.selectAll("circle");
-    var currentPositions = [];
-    currentCircles.forEach(function (circle) {
-      currentPositions.push({
-        cx: parseFloat(circle.attr("cx")),
-        cy: parseFloat(circle.attr("cy"))
-      });
-    });
-
-    // Fetch all circles and positions from the target SVG
-    var targetCircles = targetSnap.selectAll("circle");
-    var targetPositions = [];
-    targetCircles.forEach(function (circle) {
-      targetPositions.push({
-        cx: parseFloat(circle.attr("cx")),
-        cy: parseFloat(circle.attr("cy"))
-      });
-    });
-
-    currentCircles.forEach(function (circle, index) {
-      circle.addClass('animate-circle'); // Add a class for animation
-    });
-
-    // Animate circle positions from current to target positions with a 3-second duration
-    Snap.animate(currentPositions, targetPositions, function (value) {
-      currentCircles.forEach(function (circle, index) {
-        circle.attr({ cx: value[index].cx, cy: value[index].cy });
-      });
-    }, 3000, mina.easeout, function() {
-      // Animation completed
-      currentSnap.attr({ fill: "none" });
-      targetSnap.attr({ fill: "black" });
+    // Wait for the animation to finish
+    setTimeout(function () {
+      // Remove animation class
+      currentBackground.classList.remove('animate-background');
+      // Set target background to black
+      targetBackground.style.fill = "black";
+      // Set container background image
+      var contentElement = document.querySelector('.container');
       contentElement.style.backgroundImage = "url('path/to/your/svg/file.svg')"; // Replace 'path/to/your/svg/file.svg' with the actual path to your target SVG
-    });
+    }, 3000); // 3000ms is the animation duration, adjust as needed
   }
 });
