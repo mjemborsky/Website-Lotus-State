17ac20a1edbf76898af071b5a86968fbd23b9d54
mjemborsky <michaelemborsky@gmail.com>
2023-07-27 11:30:36 -0400

    Update pageTransitions.js

--- pageTransitions.js
+++ pageTransitions.js
@@ -42,7 +42,7 @@ function extractCirclePositions(svgRoot) {
 document.addEventListener('DOMContentLoaded', function () {
   // BACKGROUND ANIMATION //
   // Initializing Links
-  const index = document.querySelector('.header-text');
+  const home = document.querySelector('.header-text');
   const projects = document.querySelectorAll('.link-left');
   const more = document.querySelector('.link-right');
   const container = document.querySelector('.container');
@@ -55,14 +55,6 @@ document.addEventListener('DOMContentLoaded', function () {
   ];
   let preloadedSVGs = JSON.parse(sessionStorage.getItem('preloadedSVGs'));
   
-
-  container.classList.add('fade-in');
-  // Reset the opacity to 1 after the fade-in effect is complete
-  setTimeout(function () {
-    container.classList.remove('fade-in');
-  }, 2000); // Wait for 2 seconds (the same duration as fade-out) before removing 'fade-in' class
-
-
   if (!preloadedSVGs) {
     preloadedSVGs = [];
 
@@ -97,7 +89,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Event Listeners
   // Background: index
-  index.addEventListener('click', function(i) {
+  home.addEventListener('click', function(i) {
     var targetBackground = preloadedSVGs[0];
     animateBackground(currentBackground, targetBackground);
   });
@@ -170,3 +162,145 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  // BACKGROUND ANIMATION //
+  // Initializing Links
+  const index = document.querySelector('.header-text');
+  const projects = document.querySelectorAll('.link-left');
+  const more = document.querySelector('.link-right');
+  const container = document.querySelector('.container');
+  const svgUrls = [
+    'backgroundOne.svg',
+    'backgroundTwo.svg',
+    'backgroundFive.svg',
+    'backgroundThree.svg',
+    'backgroundFour.svg'
+  ];
+  let preloadedSVGs = JSON.parse(sessionStorage.getItem('preloadedSVGs'));
+  
+  if (!preloadedSVGs) {
+    preloadedSVGs = [];
+
+    function preloadSVGs(urls) {
+      let remaining = urls.length;
+
+      function preloadNext() {
+        if (remaining === 0) {
+          // All SVGs have been preloaded
+          sessionStorage.setItem('preloadedSVGs', JSON.stringify(preloadedSVGs));
+          console.log('All SVGs preloaded:', preloadedSVGs);
+          return;
+        }
+        const url = urls[urls.length - remaining];
+        getSVGContent(url, function (svgRoot) {
+          preloadedSVGs.push(svgRoot);
+          remaining--;
+          // Call the next preload iteration
+          preloadNext();
+        });
+      }
+      preloadNext();
+    }
+    preloadSVGs(svgUrls);
+  } else {
+    console.log('SVGs already preloaded:', preloadedSVGs);
+  }
+
+  var currentObject = document.querySelector('.background-svg');
+  var currentIndex = currentObject.getAttribute('index')
+  var currentBackground = preloadedSVGs[currentIndex];
+
+  // Event Listeners
+  // Background: index
+  index.addEventListener('click', function(i) {
+    var targetBackground = preloadedSVGs[0];
+    animateBackground(currentBackground, targetBackground);
+  });
+  // Background: projects
+  projects.forEach(function(link) {
+    link.addEventListener('click', function(i) {
+      var targetBackground = preloadedSVGs[1];
+      animateBackground(currentBackground, targetBackground);
+    });
+  });
+  // Background: more
+  more.addEventListener('click', function(i) {
+    var targetBackground = preloadedSVGs[2];
+    animateBackground(currentBackground, targetBackground);
+  });
+
+  // Add a flag to indicate if an animation is currently in progress
+  let animationInProgress = false;
+  const animationQueue = []; // Store the queued animations
+
+  function animateBackground(currentBackground, targetBackground) {
+    // If an animation is already in progress, queue the new animation
+    if (animationInProgress) {
+      animationQueue.push(targetBackground);
+      return;
+    }
+    // Mark animation as in progress
+    animationInProgress = true;
+
+    const currentCircles = extractCirclePositions(currentBackground);
+
+    // Animate the circles' positions
+    getSVGContent(targetBackground, function (targetSvgRoot) {
+      // Assuming each circle has the class 'circle' in your SVGs
+      const targetCircles = extractCirclePositions(targetSvgRoot);
+      // Ensure the number of circles is the same in both SVGs
+      if (currentCircles.length !== targetCircles.length) {
+        console.error('The number of circles in the SVGs is different!');
+        return;
+      }
+      const duration = 3000; // 3 seconds
+      const startTime = performance.now();  
+
+      function updatePositions(timestamp) {
+        const progress = (timestamp - startTime) / duration;
+        if (progress < 1) {
+          animationFrameId = requestAnimationFrame(updatePositions);
+        } else {
+          // Animation is complete
+          currentBackground = targetBackground;
+          // Mark animation as complete
+          animationInProgress = false;
+
+          // If there are queued animations, start the next one
+          if (animationQueue.length > 0) {
+            const nextBackground = animationQueue.shift();
+            animateBackground(currentBackground, nextBackground);
+          }
+        }
+        currentCircles.forEach((currentCircle, index) => {
+          const targetCircle = targetCircles[index];
+          const currentX = Number(currentCircle.getAttribute('cx'));
+          console.log('currentX')
+          const currentY = Number(currentCircle.getAttribute('cy'));
+          const targetX = Number(targetCircle.getAttribute('cx'));
+          const targetY = Number(targetCircle.getAttribute('cy'));
+
+          const interpolatedX = currentX + (targetX - currentX) * progress;
+          const interpolatedY = currentY + (targetY - currentY) * progress;
+
+          currentCircle.setAttribute('cx', interpolatedX);
+          currentCircle.setAttribute('cy', interpolatedY);
+        });
+
+        if (progress < 1) {
+          requestAnimationFrame(updatePositions);
+        }
+      }
+      }
+
+      // ... (existing code)
+
+    // Start the animation loop and store the animation frame ID
+    animationFrameId = requestAnimationFrame(updatePositions);
+
+    // ... (existing code)
+  }
+
+  // ... (existing code)
+});
