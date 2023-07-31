16290774a3da419e51c3dd87279b9034928e2556
mjemborsky <michaelemborsky@gmail.com>
2023-07-31 19:36:05 -0400

    Update pageTransitions.js

--- pageTransitions.js
+++ pageTransitions.js
@@ -15,41 +15,67 @@ async function getAllSnapSVG(svgUrl) {
 }
 
 
+async function getAllSVG(svgUrl) {
+  const cachedSVG = sessionStorage.getItem(svgUrl);
+  if (cachedSVG) {
+    const parser = new DOMParser();
+    const svgDoc = parser.parseFromString(cachedSVG, 'image/svg+xml');
+    const svgRoot = svgDoc.documentElement;
+    return svgRoot;
+  } else {
+    try {
+      const response = await fetch(svgUrl);
+      const svgContent = await response.text();
+      sessionStorage.setItem(svgUrl, svgContent); // Cache the SVG content in SessionStorage
+      const parser = new DOMParser();
+      const svgDoc = parser.parseFromString(svgContent, 'image/svg+xml');
+      const svgRoot = svgDoc.documentElement;
+      return svgRoot;
+    } catch (error) {
+      return null;
+    }
+  }
+}
+
+
 async function preloadSVGs(urls) {
   let remaining = urls.length;
-  async function preloadNext() { // Add 'async' here
+  function preloadNext() {
     if (remaining === 0) {
       return;
     }
     const url = urls[urls.length - remaining];
-    try { // Add try-catch block for better error handling
-      await getAllSnapSVG(url);
-    } catch (error) {
-      console.error('Error preloading SVG:', error);
-    }
-    remaining--;
-    // Call the next preload iteration
-    preloadNext();
+    getAllSVG(url)
+      .then((svgRoot) => {
+        remaining--;
+        // Call the next preload iteration
+        preloadNext();
+      })
+      .catch((error) => {
+        console.error('Error preloading SVG:', error);
+        remaining--;
+        // Call the next preload iteration
+        preloadNext();
+      });
   }
   preloadNext();
 }
 
 function getStoredSVG(url) {
   const svgString = sessionStorage.getItem(url);
-  if (!svgString) {
-    return null; // SVG not found in SessionStorage, handle this case accordingly
-  }
-  return Snap.parse(svgString);
+  const parser = new DOMParser();
+  const svgDoc = parser.parseFromString(svgString, 'image/svg+xml');
+  return svgDoc.documentElement;
 }
 
-// Function to animate circle positions
+// Function to animate circle positions using CSS transitions
 function animateBackground(currentBackground, targetBackground) {
   // Parse circle elements from the currentBackground SVG
   const currentCircles = currentBackground.querySelectorAll('circle');
   // Parse circle elements from the targetBackground SVG
   const targetCircles = targetBackground.querySelectorAll('circle');
-  
-  // Animate the positions of the circles
+
+  // Animate the positions of the circles using CSS transitions
   for (let i = 0; i < currentCircles.length; i++) {
     const currentCircle = currentCircles[i];
     const targetCircle = targetCircles[i];
@@ -62,29 +88,15 @@ function animateBackground(currentBackground, targetBackground) {
     const targetX = targetCircle.getAttribute('cx');
     const targetY = targetCircle.getAttribute('cy');
 
-    // Create a new circle element for the animation
-    const animatedCircle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    animatedCircle.setAttribute('r', currentCircle.getAttribute('r'));
-    animatedCircle.setAttribute('cx', initialX);
-    animatedCircle.setAttribute('cy', initialY);
-    animatedCircle.setAttribute('fill', currentCircle.getAttribute('fill'));
-    animatedCircle.setAttribute('stroke', currentCircle.getAttribute('stroke'));
-    animatedCircle.setAttribute('stroke-width', currentCircle.getAttribute('stroke-width'));
+    // Apply CSS transition to animate the circle position over 3 seconds
+    currentCircle.style.transition = 'all 3s ease-out';
+    currentCircle.style.cx = targetX;
+    currentCircle.style.cy = targetY;
 
-    // Append the animated circle to the existing container
-    document.querySelector('.container').appendChild(animatedCircle);
-
-    // Animate the circle position over 3 seconds
-    animatedCircle.animate(
-      [
-        { cx: initialX, cy: initialY },
-        { cx: targetX, cy: targetY }
-      ],
-      {
-        duration: 3000,
-        easing: 'ease-out'
-      }
-    );
+    // Remove the transition after the animation is complete
+    setTimeout(() => {
+      currentCircle.style.transition = '';
+    }, 3000);
   }
 
   // After the animation, remove the currentBackground and targetBackground SVGs
