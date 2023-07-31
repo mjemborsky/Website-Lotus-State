eaf99d093ba16d2a72322c9ddbb0d54837f13d2b
mjemborsky <michaelemborsky@gmail.com>
2023-07-31 16:17:39 -0400

    Update pageTransitions.js

--- pageTransitions.js
+++ pageTransitions.js
@@ -1,83 +1,65 @@
-async function getAllSVG(svgUrl) {
+async function getAllSnapSVG(svgUrl) {
   const cachedSVG = sessionStorage.getItem(svgUrl);
   if (cachedSVG) {
-    const parser = new DOMParser();
-    const svgDoc = parser.parseFromString(cachedSVG, 'image/svg+xml');
-    const svgRoot = svgDoc.documentElement;
-    return svgRoot;
+    return Snap.parse(cachedSVG);
   } else {
     try {
       const response = await fetch(svgUrl);
       const svgContent = await response.text();
       sessionStorage.setItem(svgUrl, svgContent); // Cache the SVG content in SessionStorage
-      const parser = new DOMParser();
-      const svgDoc = parser.parseFromString(svgContent, 'image/svg+xml');
-      const svgRoot = svgDoc.documentElement;
-      return svgRoot;
+      return Snap.parse(svgContent);
     } catch (error) {
       return null;
     }
   }
 }
 
-async function preloadSVGs(urls) {
-  let remaining = urls.length;
-  function preloadNext() {
-    if (remaining === 0) {
-      return;
-    }
-    const url = urls[urls.length - remaining];
-    getAllSVG(url)
-      .then((svgRoot) => {
-        remaining--;
-        // Call the next preload iteration
-        preloadNext();
-      })
-      .catch((error) => {
-        console.error('Error preloading SVG:', error);
-        remaining--;
-        // Call the next preload iteration
-        preloadNext();
-      });
+function preloadNext(urls) {
+  if (remaining === 0) {
+    return;
   }
-  preloadNext();
+  const url = urls[urls.length - remaining];
+  getAllSnapSVG(url)
+    .then((svgRoot) => {
+      remaining--;
+      // Call the next preload iteration
+      preloadNext();
+    })
+    .catch((error) => {
+      console.error('Error preloading SVG:', error);
+      remaining--;
+      // Call the next preload iteration
+      preloadNext();
+    });
 }
 
 function getStoredSVG(url) {
   const svgString = sessionStorage.getItem(url);
-  const parser = new DOMParser();
-  const svgDoc = parser.parseFromString(svgString, 'image/svg+xml');
-  return svgDoc.documentElement;
+  if (!svgString) {
+    return null; // SVG not found in SessionStorage, handle this case accordingly
+  }
+  return Snap.parse(svgString);
 }
-
 
 function animateBackground(currentBackground, targetBackground) {
   const container = document.querySelector('.container');
   container.classList.add('animating');
 
-  var currentContainer = Snap(container);
-  var currentSvg = Snap(currentBackground);
-  var targetSvg = Snap(targetBackground);
-
-  container.style.background = 'none';
-  container.appendChild(currentBackground);
-
-  var currentCircles = currentSvg.selectAll("circle");
-  console.log(currentCircles);
-  var targetCircles = targetSvg.selectAll("circle");
-  console.log(targetCircles);
+  const currentContainer = Snap(container);
+  const currentCircles = currentBackground.selectAll("circle");
+  const targetCircles = targetBackground.selectAll("circle");
 
   if (currentCircles.length !== targetCircles.length) {
     console.error("The number of circles in the SVGs should be the same.");
     return;
   }
 
-  var animationDuration = 3000; // 3 seconds
+  const animationDuration = 3000; // 3 seconds
 
   currentCircles.forEach(function (currentCircle, index) {
-    var targetCircle = targetCircles[index];
-    var targetRadius = targetCircle.attr("r");
-    var targetFill = targetCircle.attr("fill");
+    const targetCircle = targetCircles[index];
+    const targetRadius = targetCircle.attr("r");
+    const targetFill = targetCircle.attr("fill");
 
     currentCircle.animate(
       {
@@ -88,7 +70,8 @@ function animateBackground(currentBackground, targetBackground) {
       mina.easeinout
     );
   });
-  currentBackground = targetBackground;
+
+  currentContainer.append(targetBackground);
   container.classList.remove('animating');
 }
 
@@ -131,20 +114,28 @@ document.addEventListener('DOMContentLoaded', function () {
   // Event Listeners
   // Background: index
   home.addEventListener('click', function(i) {
-    var targetBackground = getStoredSVG('backgroundOne.svg');
-    animateBackground(currentBackground, targetBackground);
+    getAllSnapSVG('backgroundOne.svg').then((targetBackground) => {
+      animateBackground(currentBackground, targetBackground);
+      currentBackground = targetBackground;
+    });
   });
+
   // Background: projects
   projects.forEach(function(link) {
     link.addEventListener('click', function(i) {
-      var targetBackground = getStoredSVG('backgroundTwo.svg');
-      animateBackground(currentBackground, targetBackground);
+      getAllSnapSVG('backgroundTwo.svg').then((targetBackground) => {
+        animateBackground(currentBackground, targetBackground);
+        currentBackground = targetBackground;
+      });
     });
   });
+
   // Background: more
   more.addEventListener('click', function(i) {
-    var targetBackground = getStoredSVG('backgroundFive.svg');
-    animateBackground(currentBackground, targetBackground);
+    getAllSnapSVG('backgroundFive.svg').then((targetBackground) => {
+      animateBackground(currentBackground, targetBackground);
+      currentBackground = targetBackground;
+    });
   });
 });
 
