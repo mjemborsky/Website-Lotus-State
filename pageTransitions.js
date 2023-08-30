7503fe465f6c1ab13962321f4eb1b44d8e3bb00d
mjemborsky <michaelemborsky@gmail.com>
2023-08-30 15:50:01 -0400

    changes

--- pageTransitions.js
+++ pageTransitions.js
@@ -1,3 +1,11 @@
+// Preload SVGs for Background
+const svgUrls = [
+  'backgroundOne.svg',
+  'backgroundTwo.svg',
+  'backgroundFive.svg',
+  'backgroundThree.svg',
+  'backgroundFour.svg'
+];
 
 // Preload SVGs
 async function preloadSVGs(urls) {
@@ -23,69 +31,50 @@ function getStoredSVG(url) {
   return svgDoc.documentElement;
 }
 
-// Preload SVGs for Background
-const svgUrls = [
-  'backgroundOne.svg',
-  'backgroundTwo.svg',
-  'backgroundFive.svg',
-  'backgroundThree.svg',
-  'backgroundFour.svg'
-];
-
-// Assuming you have a function to handle page transitions
-function handlePageTransition(targetPage) {
-  // Apply fade-out animation to the current container
-  const currentContainer = document.querySelector('.container');
-  currentContainer.classList.add('fade-out');
-  
-  // Listen for the animationend event to trigger page navigation
-  currentContainer.addEventListener('animationend', function() {
-    // Remove the animation class
-    currentContainer.classList.remove('fade-out');
-
-    // Navigate to the target page after a delay
-    setTimeout(function() {
-      window.location.href = targetPage;
-
-      // Apply fade-in animation to the next container after another delay
-      setTimeout(function() {
-        const futureContainer = document.querySelector('.next-container');
-        futureContainer.classList.add('fade-in');
-      }, 2000); // 2 seconds delay
-    }, 2000); // 2 seconds delay
-  });
+function handlePageTransition() {
+  // Items to be applied fade in/out
+  const contents = document.querySelectorAll('.container > *:not(svg)');
+  // Apply fade-out animation to the current container (needs a timeout/delay to ensure 2 second transition here)
+  container.classList.add('fade-out');
+  // Needs to then remove the fade out effect after 2 seconds
+  setTimeout(function() {
+    container.classList.remove('fade-out');
+  }, 2000);
 }
 
-
+// MAIN FUNCTION
 // Preload SVGs before setting up event listeners and animations
 preloadSVGs(svgUrls).then(() => {
   // Setup event listeners after preloading
   document.addEventListener('DOMContentLoaded', function () {
-    var container = document.querySelector('.container');
+    const container = document.querySelector('.container');
+    const contents = document.querySelectorAll('.container > *:not(svg)');
     const home = document.querySelector('.header-text');
     const projects = document.querySelectorAll('.link-left');
     const more = document.querySelector('.link-right');
 
+    contents.classList.add('fade-in');
+
     // Event listener and animation for links
-    function handleLinkClick(targetBackgroundUrl, nextContainer) {
-      handlePageTransition(targetBackgroundUrl, nextContainer);
+    function handleLinkClick() {
+      handlePageTransition();
     }
 
     // Event listener and animation for Home link
     home.addEventListener('click', function (event) {
-      handleLinkClick('backgroundOne.svg', "container-one");
+      handleLinkClick();
     });
 
     // Event listeners and animations for Projects links
     projects.forEach(link => {
       link.addEventListener('click', function (event) {
-        handleLinkClick('backgroundTwo.svg', "container-two");
+        handleLinkClick();
       });
     });
 
     // Event listener and animation for More link
     more.addEventListener('click', function (event) {
-      handleLinkClick('backgroundFive.svg', "container-five");
+      handleLinkClick();
     });
   });
 });
