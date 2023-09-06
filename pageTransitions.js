2cf109cf050af105cffbac7c329ff23b36af9800
mjemborsky <michaelemborsky@gmail.com>
2023-09-06 15:02:09 -0400

    Update pageTransitions.js

--- pageTransitions.js
+++ pageTransitions.js
@@ -28,36 +28,44 @@ function getStoredSVG(url) {
   const svgDoc = parser.parseFromString(svgString, 'image/svg+xml');
   return svgDoc.documentElement;
 }
-// Handle page transition including fade and call to translate circles
+// Handle page transition including fade and AJAX loading
 function handlePageTransition(destinationURL) {
   const content = document.querySelector('.fade-target');
   content.classList.add('fade-out'); // Add fade-out class to trigger fade-out animation
+  setTimeout(function () {
+    // Remove fade-out class after animation duration
+    container.classList.remove('fade-out');
+    content.style.opacity = '0';
+  }, 2000);
   // Fetch the new page content using AJAX
   fetch(destinationURL)
     .then(response => response.text())
-    .then(nextPage => {
+    .then(content => {
       setTimeout(function () {
         // Remove fade-out class after animation duration
-        content.classList.remove('fade-out');
-        content.style.opacity = '0';
+        container.classList.remove('fade-out');
+        contents.forEach(content => {
+          content.style.opacity = '0';
+        });
+
         // Replace the container content with the new page content
-        container.innerHTML = nextPage;
-        const newContent = container.querySelector('.fade-target');
+        container.innerHTML = content;
+
         // Apply fade-in animation to the new content
-        newContent.classList.add('fade-in');
+        container.classList.add('fade-in');
         setTimeout(function () {
+          container.classList.remove('fade-in');
+
           // Set opacity back to 1 for all contents
-          newContent.classList.remove('fade-in');
-          newContent.style.opacity = '1';
+          const newContents = container.querySelectorAll('*:not(svg)');
+          newContents.forEach(content => {
+            content.style.opacity = '1';
+          });
         }, 2000); // 2 seconds for fade-in
       }, 2000); // 2 seconds for fade-out
     })
     .catch(error => {
       console.error('Error loading page:', error);
-    })
-    .finally(() => {
-      // Move to next page
-      window.location.href = destinationURL;
     });
 }
 // MAIN PAGE LISTENER
