e2cecee3a1600b6a1634da490f100bf79348be36
mjemborsky <michaelemborsky@gmail.com>
2023-09-14 15:55:22 -0400

    File structure changes, new files for idle animations

---
+++ idleCircles.js
@@ -0,0 +1,7 @@
+// WHEN ON PROJECT PAGES OR EXTRA.HTML
+// When on projects...
+// Animate background circles back and forth
+// between backgroundTwo.svg and backgroundThree.svg
+// When on extra...
+// Animate background circles back and forth
+// between backgroundFive.svg and backgroundFour.svg
---
+++ idleHome.js
@@ -0,0 +1,5 @@
+// WHEN ON INDEX.HTML (Homepage)
+// Implement unique idle animation, I am thinking like
+// some sparkly, iridescent fluttery lights/patterns that will
+// appear randomly overlayed on the background, kind of like
+// embers from a fire or a similar effect
