dfffe48bc45e60b3a6ecb3710fcf0a6cf1ef7081
mjemborsky <michaelemborsky@gmail.com>
2023-10-23 14:31:26 -0400

    Update idleAnimations.js

--- idleAnimations.js
+++ idleAnimations.js
@@ -1,8 +1,23 @@
+// STILL NEED TO DO
+// - Fade in effect when page FIRSTS loads
+// - Fix circle transitions - Animation length is being cut off AND fade in occuring after animation end
+// - Both Idle animations
+// - All media queries
+// - Clean up
+
+
+
+
+
 // WHEN ON INDEX.HTML (Homepage)
+// IDEA: Maybe when index.html first loads, FOR THE FIRST TIME, loads from black and all elements fade in
 // Implement unique idle animation, I am thinking like
 // some sparkly, iridescent fluttery lights/patterns that will
 // appear randomly overlayed on the background, kind of like
 // embers from a fire or a similar effect
+// This effect will most likely have to be a video file, but it needs to have a transparent
+// background, and be overlayed over everything, with opacity down
+// Could create in after effects, and COULD be a svg but animating the svg would be not good for performance
 
 // WHEN ON PROJECT PAGES OR EXTRA.HTML
 // Needs to wait for page to load
@@ -14,12 +29,6 @@
 // Then the code should call the function in reverse, so that it animates the circle positions back to the current svg over 4 seconds, same ease
 
 
-// IDEA: Maybe when index.html first loads, FOR THE FIRST TIME, loads from black and all elements fade in
-
-
-
-// NEED Function - SVG
-// Handle page transition including fade and AJAX loading
 
 // isIdle = true;
     // // Check if the currentSVG matches a specific SVG filename
@@ -40,12 +49,4 @@
     //   animateCircles(targetSVG);
     //   animateCircles(currentSVG);
     // }
-
-
-// STILL NEED TO DO
-// - Fade in effect when page FIRSTS loads
-// - Fix circle transitions - Animation length is being cut off AND fade in occuring after animation end
-// - Both Idle animations
-// - All media queries
-// - Clean up
 
