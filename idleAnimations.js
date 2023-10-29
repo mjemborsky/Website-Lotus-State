4443b30d790c03e91982e8c4ccec4b729f8014e4
mjemborsky <33811013+mjemborsky@users.noreply.github.com>
2023-10-28 20:12:22 -0400

    revert

--- idleAnimations.js
+++ idleAnimations.js
@@ -40,37 +40,37 @@
     //   animateCircles(currentSVG);
     // }
 
-    document.addEventListener('DOMContentLoaded', function () {
-        // Get all the circle elements within the SVG
-        const circles = document.querySelectorAll("svg circle");
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     // Get all the circle elements within the SVG
+    //     const circles = document.querySelectorAll("svg circle");
     
-        // Set the animation parameters
-        const animationDuration = 10000; // Animation duration in milliseconds
-        const initialY = 938; // Initial vertical position
-        const finalY = -50; // Final vertical position
-        const velocity = (finalY - initialY) / animationDuration; // Calculate velocity
+    //     // Set the animation parameters
+    //     const animationDuration = 10000; // Animation duration in milliseconds
+    //     const initialY = 938; // Initial vertical position
+    //     const finalY = -50; // Final vertical position
+    //     const velocity = (finalY - initialY) / animationDuration; // Calculate velocity
     
-        // Define the animation function
-        function animateBubbles(timestamp) {
-            circles.forEach((circle) => {
-                if (!circle.startTime) {
-                    circle.startTime = timestamp;
-                }
+    //     // Define the animation function
+    //     function animateBubbles(timestamp) {
+    //         circles.forEach((circle) => {
+    //             if (!circle.startTime) {
+    //                 circle.startTime = timestamp;
+    //             }
     
-                const elapsed = timestamp - circle.startTime;
-                const newY = initialY + velocity * elapsed;
+    //             const elapsed = timestamp - circle.startTime;
+    //             const newY = initialY + velocity * elapsed;
     
-                // Reset the circle's position when it reaches the top
-                if (newY < finalY) {
-                    circle.startTime = timestamp; // Reset the start time for the circle
-                } else {
-                    circle.setAttribute("cy", newY);
-                }
-            });
+    //             // Reset the circle's position when it reaches the top
+    //             if (newY < finalY) {
+    //                 circle.startTime = timestamp; // Reset the start time for the circle
+    //             } else {
+    //                 circle.setAttribute("cy", newY);
+    //             }
+    //         });
     
-            requestAnimationFrame(animateBubbles);
-        }
+    //         requestAnimationFrame(animateBubbles);
+    //     }
     
-        // Start the animation loop
-        requestAnimationFrame(animateBubbles);
-    });
+    //     // Start the animation loop
+    //     requestAnimationFrame(animateBubbles);
+    // });
