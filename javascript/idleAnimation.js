04e25056ce2b697b591e0656380c8e8e51d0b922
mjemborsky <michaelemborsky@gmail.com>
2023-11-01 13:43:48 -0400

    comments

--- javascript/idleAnimation.js
+++ javascript/idleAnimation.js
@@ -1,57 +1,20 @@
-// WHEN ON INDEX.HTML (Homepage)
-// IDEA: Maybe when index.html first loads, FOR THE FIRST TIME, loads from black and all elements fade in
-// Implement unique idle animation, I am thinking like
-// some sparkly, iridescent fluttery lights/patterns that will
-// appear randomly overlayed on the background, kind of like
-// embers from a fire or a similar effect
-// This effect will most likely have to be a video file, but it needs to have a transparent
-// background, and be overlayed over everything, with opacity down
-// Could create in after effects, and COULD be a svg but animating the svg would be not good for performance
-// It honestly might not require any javascript, and could be set up with css and have autoplay on
+// LAST THINGS TO DO (FOR WEBSITE RESPONSIVENESS (NOT CONTENT))
 
-// WHEN ON PROJECT PAGES OR EXTRA.HTML
-// Needs to wait for page to load
-// Then it identifies the active svg and current points,
-// then identifies the target svg based off the current (projects or extra)
-// then WHILE the page is opened and not navigating,
-// it should call a function, idleCircles that accepts one svg (target)
-// It should animate the r values of the current svg to this new svg positions over 4 seconds, with no ease in or out (linear velocity)
-// Then the code should call the function in reverse, so that it animates the circle positions back to the current svg over 4 seconds, same ease
-// Honestly might be better to include in pageTransitions as it could be linked to the links as well as us the animation functions already described.
+// 1. Implement Idle Bubbles with fade during transition
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Get all the circle elements within the SVG
-//     const bubbleSVG = document.querySelector('svg:nth-child(2)');
-//     console.log(bubbleSVG);
-//     const bubbles = bubbleSVG.querySelectorAll('circle');
-    
-//     // Set the animation parameters
-//     const animationDuration = 10000; // Animation duration in milliseconds
-//     const initialY = 938; // Initial vertical position
-//     const finalY = -50; // Final vertical position
-//     const velocity = (finalY - initialY) / animationDuration; // Calculate velocity
-    
-//     // Define the animation function
-//     function animateBubbles(timestamp) {
-//         bubbles.forEach((bubble) => {
-//             if (!bubble.startTime) {
-//                 bubble.startTime = timestamp;
-//             }
-    
-//             const elapsed = timestamp - bubble.startTime;
-//             const newY = initialY + velocity * elapsed;
-    
-//             // Reset the circle's position when it reaches the top
-//             if (newY < finalY) {
-//                 bubble.startTime = timestamp; // Reset the start time for the circle
-//             } else {
-//                 bubble.setAttribute("cy", newY);
-//                 console.log('cy');
-//             }
-//         });
-    
-//         requestAnimationFrame(animateBubbles);
-//     }
-//     // Start the animation loop
-//     requestAnimationFrame(animateBubbles);
-// });
+// Bubbles.svg should be inside the section element so it fades with it, this SHOULD take care of the fading
+// When page is loaded...
+// Select bubbles svg
+// Call forever...
+    // AnimateBubbles function
+        // Should store all circles in the svg
+        // Circles should probably be selected with different selector than 'circle', maybe so class
+        // Animate them up the screen until they get to a full screen height from the initial position,
+        // Then reset to initial position and run again.
+        // When links are clicked, animation should continue but fade should be applied and opacity set to 0
+
+
+// 2. Enable fade/transitions when first loading page (there is currently a delay)
+
+
+// 3. Media Queries for different screen sizes and connection speeds
