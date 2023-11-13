07f58c1e17cc420cc919e0e3b34627e5f37ad70e
mjemborsky <michaelemborsky@gmail.com>
2023-11-13 14:11:41 -0500

    Update pageTransitions.js

--- javascript/pageTransitions.js
+++ javascript/pageTransitions.js
@@ -66,6 +66,7 @@ function animatePathWithDelay(paths) {
   function animateSinglePath(path, delay) {
     const startY = getInitialY(path);
     const endY = parseFloat(window.innerHeight + (window.innerHeight / 2));
+    console.log(endY);
     let startTime;
     function step(timestamp) {
       if (!startTime) startTime = timestamp;
