beb0ee60d22a2f49f91d7e89d1c909560f8c750b
Michael Emborsky <33811013+mjemborsky@users.noreply.github.com>
2024-10-08 08:16:25 -0500

    Update animations.js

--- animations.js
+++ animations.js
@@ -7,20 +7,20 @@
 // animate 2 svg's, the background circles and the idle 
 // floating 'paths'. 
 
-
-// TO START (WHEN INDEX.HTML FIRST LOADS)
+// TO DO: implement changes in animations done based on users speed and device settings to ensure clean design regardless
 // Need to TEST this performance with performance.now to determine user's machine capabilities
 // Need to establish categories of performance that determine what animation methods and how
 // much animation will be shown (2g + low cores/low performance, 3g + medium cores/medium performance, 4/5g)
 
+// Variable to check if page is hidden/visible
 let isPageHidden = false;
-// Preload SVGs for Background - Stores svg's as list of names
+// Array of Circle SVG's used
 const svgUrls = [
   'backgroundOne.svg',
   'backgroundTwo.svg',
   'backgroundFive.svg'
 ];
-// Preload SVGs
+// Function to preload SVGs
 async function preloadSVGs(urls) {
   try {
     for (const url of urls) {
@@ -48,9 +48,11 @@ function isCurrentSVG(filename) {
   const currentSVG = document.querySelector('.background-svg');
   return currentSVG.src.includes(filename);
 }
+// Function to check if user is on mobile device
 function isMobile() {
   return window.innerWidth <= 430;
 }
+// Function to clear all paths from the current page
 function clearPaths(container) {
   const group = container.querySelector('g');
   while (group.firstChild) {
@@ -77,6 +79,7 @@ function createPaths(numPaths, container) {
     group.appendChild(path);
   }
 }
+// Function for setting initial positions for each path
 function setInitialPathPositions(paths) {
   paths.forEach(path => {
     const currentTransform = path.getAttribute('transform');
@@ -86,6 +89,7 @@ function setInitialPathPositions(paths) {
     path.setAttribute('transform', `matrix(1, 0, 0, 1, ${initialX}, ${initialY})`);
   });
 }
+// Function for animating single path
 function animatePathWithDelay(paths) {
   setInitialPathPositions(paths);
   const baseAnimationDuration = 20000;
@@ -134,14 +138,13 @@ function animatePathWithDelay(paths) {
     animateSinglePath(path, initialStaggerDelay);
   });
 }
-
-// Animate Idle SVG (rain.svg)
+// Animate Idle SVG (bubbles.svg)
 function animateIdle() {
   const idle = document.getElementById("idle");
   const paths = idle.querySelectorAll('g path');
   animatePathWithDelay(paths);
 }
-
+// Checks if page is inactive/not visible, and stops and restarts idle animation
 document.addEventListener('visibilitychange', function () {
   isPageHidden = document.hidden;
   const idle = document.getElementById("idle");
@@ -179,6 +182,8 @@ function animateCircles(targetSVG) {
   }
   requestAnimationFrame(animate);
 }
+// Main function to handle page transitions: transfers content from current 
+// page to future page and triggers circle animation, starts new idle animation
 async function handlePageTransition(destinationURL, targetBackground) {
   var container = document.querySelector('.container');
   var content = document.querySelectorAll('.fade-target');
@@ -217,7 +222,6 @@ async function handlePageTransition(destinationURL, targetBackground) {
   }
 }
 // MAIN PAGE LISTENER/WINDOW ONLOAD FUNCTION TO SET UP EVENT LISTENERS
-// Preload SVGs before setting up link event listeners
 preloadSVGs(svgUrls).then(() => {
   window.onload = function() {
     const home = document.querySelector('.header-text');
@@ -225,28 +229,24 @@ preloadSVGs(svgUrls).then(() => {
     const more = document.querySelector('.link-right');
     var content = document.querySelectorAll('.fade-target');
     const idle = document.getElementById("idle");
-    // Add fade-in class to trigger fade-in animation
     setTimeout(() => {
       content.forEach((element) => {
         element.style.opacity = '1';
       });
-    }, 100); // slight delay for browser rendering
+    }, 100);
     // PROJECT LINK EXPANSION //
     // Initializing Properties
     const leftLink = document.querySelector('.left-link');
     const expandedLinks = document.querySelector('.expanded-links');
     expandedLinks.style.display = 'none';
-    var isExpanded = false; // Flag to track the state of expanded links
-    // Expand or Collapse Links
+    var isExpanded = false;
     leftLink.addEventListener('click', function(e) {
         e.preventDefault();
         if (isExpanded) {
-            // Collapse the links
             expandedLinks.style.display = 'none';
             isExpanded = false;
             overlay.style.opacity = '0';
         } else {
-            // Expand the links
             expandedLinks.style.display = 'flex';
             expandedLinks.style.alignItems = 'center';
             expandedLinks.style.flexDirection = 'column';
@@ -286,10 +286,10 @@ preloadSVGs(svgUrls).then(() => {
       const destinationURL = more.getAttribute('href');
       const targetBackground = getStoredSVG('backgroundFive.svg');
       handlePageTransition(destinationURL, targetBackground);
-      console.log("call page transition");
       more.blur();
     });
-    createPaths(200, idle); // Generate 100 paths dynamically
+    // Sets initial idle animation
+    createPaths(200, idle);
     animateIdle();
   };
 });
